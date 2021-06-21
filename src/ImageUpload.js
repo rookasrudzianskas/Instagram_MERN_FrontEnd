57588e53c954d629ca385e513c7151789f179e53
Rokas <rookas.rudzenskas@gmail.com>
2021-06-21 12:18:50 +0300

    Rokas

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -3,6 +3,7 @@ import firebase from "firebase";
 import { storage, db } from "./firebase";
 import "./ImageUpload.css";
 import { Input, Button } from "@material-ui/core";
+import axios from "./axios";
 
 const ImageUpload = ({ username }) => {
   const [image, setImage] = useState(null);
@@ -39,6 +40,12 @@ const ImageUpload = ({ username }) => {
           .getDownloadURL()
           .then((url) => {
             setUrl(url);
+
+            axios.post('/upload', {
+               caption: caption,
+               user: username,
+               image: url,
+            });
 
             // post image inside db
             db.collection("posts").add({
